77d870b3c992024c45ee73f321fe142713e4b5fb
Martin Chavez <martinchavez1203@gmail.com>
2023-08-01 16:05:00 -0600

    add new array cmd

--- arrays.rb
+++ arrays.rb
@@ -6,13 +6,21 @@ floats_array = [12.4,3.4,60.2,34.6]
 boolean_array = [true,false,true ,false]
 
 names.push("Juan")
-# This will add the name "Juan" to the list of elements in the names array 
+# This will add the name "Juan" to the list of elements in the names array, .push to array list
 
 nums.shift
 # This will pull the first element in the array out, which will take 30 out
 
 floats_array.pop
-# This will pull the last element in the array which would be 34.6
+# This will pull the last element in the array, which would be 34.6
 
 boolean-array.unshift(false)
 # This will add elements to the left side of the array, in this case it would add false to the left side of the boolean array. 
+
+## In the array examples above I used included all the methis we learend and explained what they do and how they will affect my arrays!
+
+
+nums = [30,90,89,76]
+nums = nums.reverse
+# This should puts "[76,89,90,30]"
+# The command .reverse flips the arrays from the last element being last to being first in the array.
