ef524b5afcfe158e2e4e3c957f42095afa1f7c73
Martin Chavez <martinchavez1203@gmail.com>
2023-08-01 15:48:16 -0600

    add array methods

--- arrays.rb
+++ arrays.rb
@@ -4,3 +4,15 @@ names = ["Martin","Rodrigo","Ezequiel","Bob"]
 nums = [30,90,89,76]
 floats_array = [12.4,3.4,60.2,34.6]
 boolean_array = [true,false,true ,false]
+
+names.push("Juan")
+# This will add the name "Juan" to the list of elements in the names array 
+
+nums.shift
+# This will pull the first element in the array out, which will take 30 out
+
+floats_array.pop
+# This will pull the last element in the array which would be 34.6
+
+boolean-array.unshift(false)
+# This will add elements to the left side of the array, in this case it would add false to the left side of the boolean array. 
